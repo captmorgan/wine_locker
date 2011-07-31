7b4a5bdcd7aad1f499090fcd2c5545c9c7d39e70
Scott Morgan <morgandev@gmail.com>
2011-07-31 08:46:56 -0700

    baseline checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WineLocker::Application.config.secret_token = 'baf5fb26d0a75aa3eb1a159ce0a9d1c299f71501e99ce7918afa8e5f85841eeef9bd72d2a6da9765af02679c38dc8bd75615d43e576a9d8355a98bf1221ce52b'
